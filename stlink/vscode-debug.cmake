2feae5d14254875b4b7101260bfd12e3af95d790
Dmitriy Zakharkin <mail@zdima.net>
2025-06-18 07:29:15 -0400

    handle existing launch.json file

--- stlink/vscode-debug.cmake
+++ stlink/vscode-debug.cmake
@@ -7,9 +7,16 @@ endforeach()
 function(vscode_debug PROJ_NAME)
     set(PROJ_ELF_PATH "${CMAKE_BINARY_DIR}/${PROJ_NAME}.elf")
     file(MAKE_DIRECTORY "${VSCODE_DIR}")
+    if(NOT EXISTS "${VSCODE_DIR}/launch.json")
+        set(LAUNCH_JSON "${VSCODE_DIR}/launch.json")
+    else()
+        message(STATUS "vscode-debug: launch.json already exists, generating file ${VSCODE_DIR}/launch.json.st-link ...")
+        message(STATUS "vscode-debug: You can copy or merge with ${VSCODE_DIR}/launch.json")
+        set(LAUNCH_JSON "${VSCODE_DIR}/launch.json.st-link")
+    endif()
     configure_file(
         "${STLINK_DIR}/vscode-debug.in"
-        "${VSCODE_DIR}/launch.json"
+        "${LAUNCH_JSON}"
         @ONLY
     )
 endfunction()
